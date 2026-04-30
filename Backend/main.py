e975a392b90392dc4aedcad0ab6ca261294bac13
nayangaripelly <garipellysainayan@gmail.com>
2026-04-30 12:13:39 +0530

    fixed cors error

--- Backend/main.py
+++ Backend/main.py
@@ -9,7 +9,11 @@ app = FastAPI(title="Reconciliation API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://reconciliation-pi.vercel.app/","http://localhost:5173"],
+    allow_origins=[
+        "https://reconciliation-pi.vercel.app",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
